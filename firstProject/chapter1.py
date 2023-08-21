1c0d60aa0d9f18d3faeb57ad7336f57a7735efcd
Mohammad Asif <asif.punchline@gmail.com>
2023-08-22 00:54:27 +0530

    UPDATE: added video play example

--- firstProject/chapter1.py
+++ firstProject/chapter1.py
@@ -1,5 +1,13 @@
 import cv2
-print("Package Imported")
-image = cv2.imread("resources/lena.png")
-cv2.imshow("Lena image", image)
-cv2.waitKey(0)
+print("Reading a video and displaying it")
+cap = cv2.VideoCapture("resources/1680871817378.mp4")
+
+while True:
+    success, img = cap.read()
+    cv2.imshow("video", img)
+    if cv2.waitKey(33) & 0xFF == ord('q'):
+        break
+
+# so waitkey(arg) generateds delay in milliseconds
+# it returns the ASCII value of the pressed key at which the loop breaks.
+# we are taking only 8bits (0xFF) and compare if the pressed key is 'q'
